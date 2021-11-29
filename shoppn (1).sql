91df1c6091713c8a704be94a9f121735720f7ec0
Ines Agasaro <ines.ituze@ashesi.edu.gh>
2021-11-29 12:55:47 +0000

    cart

---
+++ shoppn (1).sql
@@ -0,0 +1,376 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 29, 2021 at 01:54 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shoppn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`) VALUES
+(25, 'Artopia'),
+(26, 'Ikamba');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `p_id` int(11) NOT NULL,
+  `ip_add` varchar(50) NOT NULL,
+  `c_id` int(11) DEFAULT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`p_id`, `ip_add`, `c_id`, `qty`) VALUES
+(11, '::1', NULL, 2),
+(13, '::1', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`cat_id`, `cat_name`) VALUES
+(12, 'Hair Care'),
+(13, 'Skin Care'),
+(14, 'Lip Care'),
+(15, 'Spa Care'),
+(16, 'Rings'),
+(17, 'Bracelets'),
+(18, 'Necklaces');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
+  `customer_name` varchar(100) NOT NULL,
+  `customer_email` varchar(50) NOT NULL,
+  `customer_pass` varchar(150) NOT NULL,
+  `customer_country` varchar(30) NOT NULL,
+  `customer_city` varchar(30) NOT NULL,
+  `customer_contact` varchar(15) NOT NULL,
+  `customer_image` varchar(100) DEFAULT NULL,
+  `user_role` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`customer_id`, `customer_name`, `customer_email`, `customer_pass`, `customer_country`, `customer_city`, `customer_contact`, `customer_image`, `user_role`) VALUES
+(2, 'ange', 'ange@gmail.com', '$2y$10$TWLGy88q1orXj4ttzXAILe0TUYITzdJ7p/kTJ0BQ6dIenXEl7pNDG', 'Ghana', 'Accra', '64323217', 'Ines 1 PE.JPG', 1),
+(3, 'casey', 'casey@gmail.com', '$2y$10$Hyce0wTR5MBz2MdEeyDRSufhEZxyBAqr28wI4VLjY/t43YhlRvDKy', 'Ghana', 'Accra', '5461320', 'Ines 1 PE.JPG', 1),
+(4, 'admin', 'admin@gmail.com', '$2y$10$pqTNdyETQXaGfptiKVSpquO4ULP3rAQfrEwSiYQPKFATnUCnrkaTq', 'Ghana', 'Accra', '020974563', 'Ines 1 PE.JPG', 0),
+(6, 'Kweku Arthur', 'emmanuel.arthur@ashesi.edu.gh', '$2y$10$YrLFhRsIhDUXnbUsw7Z44OaQSCW0sV2N4CDgqV7mx1lkQUTI6S2Hq', 'Ghana', 'Accra', '020466397', '', 1),
+(8, 'Ines  Agasaro Ituze', 'ines.ituze@ashesi.edu.gh', '$2y$10$F1WLnqe5NvlHXl2u6Sjl3ueFVB56Ee1yhFtW.NlmNmuYFt89cPZYm', 'Ghana', 'Accra', '0591860712', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderdetails`
+--
+
+CREATE TABLE `orderdetails` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orderdetails`
+--
+
+INSERT INTO `orderdetails` (`order_id`, `product_id`, `qty`) VALUES
+(9, 12, 1),
+(9, 11, 1),
+(9, 13, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `invoice_no` int(11) NOT NULL,
+  `order_date` date NOT NULL,
+  `order_status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `customer_id`, `invoice_no`, `order_date`, `order_status`) VALUES
+(1, 2, 1000, '2021-11-10', 'uncompleted'),
+(3, 2, 5575, '2021-11-11', 'unfulfilled'),
+(4, 2, 8749, '2021-11-11', 'unfulfilled'),
+(6, 2, 8848, '2021-11-12', 'unfulfilled'),
+(7, 2, 4383, '2021-11-12', 'unfulfilled'),
+(8, 2, 6407, '2021-11-12', 'unfulfilled'),
+(9, 8, 7119, '2021-11-24', 'unfulfilled');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `pay_id` int(11) NOT NULL,
+  `amt` double NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `currency` text NOT NULL,
+  `payment_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`pay_id`, `amt`, `customer_id`, `order_id`, `currency`, `payment_date`) VALUES
+(1, 15000, 2, 3, 'GHC', '2021-11-11'),
+(2, 13000, 2, 4, 'GHC', '2021-11-11'),
+(3, 13000, 2, 6, 'GHC', '2021-11-12'),
+(4, 10000, 2, 7, 'GHC', '2021-11-12'),
+(5, 0, 2, 8, 'GHC', '2021-11-12'),
+(6, 23000, 8, 9, 'GHC', '2021-11-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `product_cat` int(11) NOT NULL,
+  `product_brand` int(11) NOT NULL,
+  `product_title` varchar(200) NOT NULL,
+  `product_price` double NOT NULL,
+  `product_desc` varchar(500) DEFAULT NULL,
+  `product_image` varchar(100) DEFAULT NULL,
+  `product_keywords` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`) VALUES
+(11, 12, 25, 'Choco Body Butter', 10000, 'Body butter', '../images/products/Choco-Body-Butter.jpg', 'choco'),
+(12, 15, 25, 'Strawberry Candle', 7000, 'Strawberry candle', '../images/products/Strawberry-Candle.jpg', 'candle'),
+(13, 12, 25, 'Mint Scalp Serum', 6000, 'olola', '../images/products/Mint-Scalp-Serum.jpg', 'serum'),
+(14, 15, 25, 'Honey Massage Oil', 10000, 'massage', '../images/products/Honey-Massage-Oil.jpg', 'massage'),
+(15, 14, 25, 'Espresso Lip Balm', 3000, 'lip balm', '../images/products/Espresso-Lipbalm.jpg', 'lip balm'),
+(16, 12, 25, 'Citrus Hair Butter', 12000, 'Hair Butter', '../images/products/Citrus-Hair-Butter.jpg', 'hair butter'),
+(17, 16, 26, 'Heart Beat Ring', 7000, 'This is minimalist ring is made of sterling silver. It is light, yet durable. \r\n', '../images/products/heartbeat.png', 'minimalist'),
+(18, 16, 26, 'Gate Ring', 8000, 'This minimalist ring is made of sterling silver. The ring size is adjustable, allowing you to wear it on different fingers. \r\n', '../images/products/gate.jpg', 'ring'),
+(19, 16, 26, 'Zirconia Ring', 15000, 'This ring is made of sterling silver and zirconia. Idea for an affordable engagement or promise ring.\r\n', '../images/products/zirconia.jpg', 'zirconia'),
+(20, 16, 26, 'Revolve ring', 25000, 'This ring is made of thick sterling silver sheet revolving around itself.', '../images/products/revolve.jpg', 'ring'),
+(21, 16, 26, 'Mini-Me Ring', 6000, 'This ring is made of a thin ring of sterling silver. It looks best when one wears a number of them as knuckle rings on different fingers. ', '../images/products/mini-me.jpg', 'Mini-Me'),
+(22, 16, 26, 'Loop Ring', 8000, 'This ring is made of a thin whoop of sterling silver which loops around itself. The number of whoops can vary depending on the client’s wishes.', '../images/products/loop.jpg', 'ring'),
+(23, 16, 26, 'Love Ring', 8000, 'This ring is made of a thin string of sterling silver, embellished with a hollow heart shape also made out of sterling silver. \r\n', '../images/products/love.jpg', 'ring'),
+(24, 16, 26, 'Reindeer Ring', 10000, 'This ring is made of sterling silver, with a reindeer horn design in its center', '../images/products/reindeer.jpg', 'ring'),
+(25, 16, 26, 'Bauhaus Ring I', 15000, 'This ring is made of sterling silver. Its design is inspired by the Bauhaus art and design movement ', '../images/products/bauhaus I.jpg', 'ring'),
+(26, 16, 26, 'Bauhaus Ring II', 15000, 'This ring is made of sterling silver. Its design is inspired by the Bauhaus art and design movement\r\n', '../images/products/bauhaus II.png', 'ring'),
+(27, 16, 26, 'Stacklable Ring', 20000, 'This ring is made of many sterling silver bands. The customer may customize the ring to have as many stackable bands as they want.\r\n', '../images/products/stackable.jpg', 'ring'),
+(28, 16, 26, 'Lava Tree Ring', 15000, 'This bold ring is made of a sterling silver band and a large lava rock engulfed in tree branches made of sterling silver wires. ', '../images/products/lava tree.jpg', 'ring'),
+(29, 17, 26, 'Healing Lava Bracelet', 7000, 'This bracelet is made of ring is made of lava beads and an assortment of different bead spacers. The pores in the lava beads are filled with aroma-therapeutic essential oils.\r\n', '../images/products/bracelet.jpg', 'bracelet'),
+(30, 18, 26, 'Healing lava Necklace', 15000, 'This necklace is made of a thin sterling silver chain embellished with a lava bead. This bead is filled with aroma-therapeutic essential oils. \r\n', '../images/products/healing necklace.jpg', 'Necklace');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD KEY `p_id` (`p_id`),
+  ADD KEY `c_id` (`c_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD UNIQUE KEY `customer_email` (`customer_email`);
+
+--
+-- Indexes for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`pay_id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`),
+  ADD KEY `product_cat` (`product_cat`),
+  ADD KEY `product_brand` (`product_brand`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `pay_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `orderdetails`
+--
+ALTER TABLE `orderdetails`
+  ADD CONSTRAINT `orderdetails_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+  ADD CONSTRAINT `orderdetails_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`);
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`),
+  ADD CONSTRAINT `payment_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_cat`) REFERENCES `categories` (`cat_id`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`product_brand`) REFERENCES `brands` (`brand_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- views/test.php
+++ views/test.php
@@ -1,17 +1,41 @@
 <?php
 
 require_once('../controllers/product_controller.php');
-$brands=displaycategories_controller();
-<li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown"> Shop by</a> 
-foreach($brands as $brand){
+
+<li class="onhover-div mobile-cart">
+<div><img src="../assets/images/icon/cart.png"
+        class="img-fluid blur-up lazyload" alt=""> <i
+        class="ti-shopping-cart"></i></div>
+        <span class="cart_qty_cls"><?php echo $itemNumber[0]['count']; ?></span>
+<ul class="show-div shopping-cart">
+    <?php if(count($cart)) {
+        foreach($cart as $item){
+            $p = select_one_product_controller($item['p_id']);    
+    ?>
+    <li>
+        <div class="media">
+            <a href="#"><img alt="" class="me-3"
+                    src="<?php echo "../".$p['product_image'] ?>"></a>
+            <div class="media-body">
+                <a href="#">
+                    <h4><?php echo $p['product_title'] ?></h4>
+                </a>
+                <h4><span><?php echo $item['qty'] ?> x GHS<?php echo number_format($cartTotal[0]['Result'], 2, '.', '') ?></span></h4>
+            </div>
+        </div>
+        <div class="close-circle"><a href="#"><i class="fa fa-times"
+                    aria-hidden="true"></i></a></div>
+    </li>
+    <?php }}else{ ?>
+        <h4>Your cart is empty</h4>
+    <?php }?>
+    <li>
+        <div class="total">
+            <h5>subtotal : <span>GHS<?php echo number_format($cartTotal[0]['Result'], 2, '.', '') ?></span></h5>
+        </div>
+    </li>
+    <li>
+        <div class="buttons"><a href="cart.php" class="view-cart">view
+                cart</a> <a href="checkout.php" class="checkout">checkout</a></div>
+    </li>
 ?>
-
-    <ul class="dropdown-menu">
-        
-            <li><a href="#"><?=$brand['cat_name']?></a></li>
-           
-     
-    </ul>
-
-<?php }?>
-</li>
